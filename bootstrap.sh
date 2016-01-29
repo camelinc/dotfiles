d1e091dd4bc1a9744a075b41a83d6a371caa49cf
camelinc <camelinc@users.noreply.github.com>
2016-01-29 14:36:14 +0100

    less output official Vundle package curlrc added

--- bootstrap.sh
+++ bootstrap.sh
@@ -8,7 +8,12 @@
 readonly DOTDIR="${HOME}/.dotfiles"
 readonly PROGNAME=$(basename "$0")
 tmp=$(dirname "$0")
-readonly PROGDIR=$(readlink -m "${tmp}")
+if [[ "$(uname -s)" == "Darwin" ]]; then
+  readonly PROGDIR=$(realpath "${tmp}")
+else
+  #Debian
+  readonly PROGDIR=$(readlink -m "${tmp}")
+fi
 readonly ARGS=( "$@" )
 
 info () {
@@ -28,6 +33,36 @@ fail () {
   echo ''
   exit
 }
+ask () {
+    # http://djm.me/ask
+    while true; do
+        if [ "${2:-}" = "Y" ]; then
+            prompt="Y/n"
+            default=Y
+        elif [ "${2:-}" = "N" ]; then
+            prompt="y/N"
+            default=N
+        else
+            prompt="y/n"
+            default=
+        fi
+
+        # Ask the question - use /dev/tty in case stdin is redirected from somewhere else
+        read -p "$1 [$prompt] " REPLY </dev/tty
+
+        # Default?
+        if [ -z "$REPLY" ]; then
+            REPLY=$default
+        fi
+
+        # Check if the reply is valid
+        case "$REPLY" in
+            Y*|y*) return 0 ;;
+            N*|n*) return 1 ;;
+        esac
+    done
+}
+
 
 install_powerline() {
   local fontdir="${HOME}/.fonts/"
@@ -41,10 +76,10 @@ install_powerline() {
   # shellcheck disable=SC2143
   if [[ -z $(pip list | grep powerline) ]]; then
     info 'installing powerline'
-    pip install --user git+git://github.com/Lokaltog/powerline
+    pip install --quiet --user git+git://github.com/Lokaltog/powerline
   else
     info 'updating powerline'
-    pip install --user --upgrade git+git://github.com/Lokaltog/powerline
+    pip install --quiet --user --upgrade git+git://github.com/Lokaltog/powerline
   fi
 
   if [[ ! -d ${fontdir} ]]; then
@@ -55,25 +90,45 @@ install_powerline() {
 
   src="${HOME}/.fonts/powerline-fonts/"
   if [[ ! -d ${src} ]]; then
-    git clone https://github.com/Lokaltog/powerline-fonts.git "${src}"
+    git clone https://github.com/Lokaltog/powerline-fonts.git "${src}" --quiet
   else
     cd "${src}" \
       || fail "Could not change into ${src}"
-    git pull
+    git pull --quiet
   fi
-
 
   info 'installing powerline-fonts'
   fcchache_bin=$(which fc-cache)
   if [[ ! -x "${fcchache_bin}" ]]; then
     info 'installing fontconfig'
-    sudo apt-get install -y fontconfig \
-      || fail "Could not install fontconfig"
+
+    if [[ "$(uname -s)" == "Darwin" ]]; then
+      brew install fontconfig \
+        || fail "Could not install fontconfig"
+    else
+      sudo apt-get install -y fontconfig \
+        || fail "Could not install fontconfig"
+    fi
   fi
 
-  sudo "${fcchache_bin}" -vf "${fontdir}"
+  sudo "${fcchache_bin}" -vf "${fontdir}" >/dev/null
 }
+install_peda() {
+  info 'installing gdb peda'
+  local pedadir="${HOME}/peda"
 
+  src="${HOME}/.fonts/powerline-fonts/"
+  if [[ ! -d ${pedadir} ]]; then
+    git clone https://github.com/longld/peda.git "${pedadir}" --quiet
+  else
+    cd "${pedadir}" \
+      || fail "Could not change into ${pedadir}"
+    git pull --quiet
+  fi
+
+  echo "source ~/peda/peda.py" >> "${HOME}/.gdbinit"
+
+}
 install_dotfiles () {
   info 'installing dotfiles'
 
@@ -82,7 +137,13 @@ install_dotfiles () {
     local dst
     dst="${HOME}/.$(basename "${src%.*}")"
 
-    if [[ -f "$dst" || -d "$dst" || -L "$dst" ]]; then
+    if [[ -f "$dst" || -d "$dst" ]]; then
+      if [[ ! -L "$dst" ]] && \
+        (ask "Delete $dst?" N);
+      then
+          continue
+      fi
+
       rm -rf "$dst"
       success "removed $dst"
     fi
@@ -121,38 +182,52 @@ do_dotfilerepo() {
       || fail "Could not change into ${DOTDIR}"
 
     info "Updating ${DOTDIR}"
-    git pull origin master \
+    git pull origin master --quiet \
       || fail "Could not update the repository: ${DOTDIR}"
 
     #update submodule
     cd antigen \
       || fail "Could not change into 'antigen'"
-    git submodule update --init --recursive
+    git submodule update --quiet --init --recursive
   else
     info "Cloning ${DOTDIR}"
-    git clone --recursive https://github.com/camelinc/Dotfiles.git "${DOTDIR}" \
+    # Quiet option not respected for submodules
+    #git clone --quiet --recursive https://github.com/camelinc/Dotfiles.git "${DOTDIR}" \
+    git clone --quiet https://github.com/camelinc/Dotfiles.git "${DOTDIR}" \
       || fail "Could not clone the repository to ${DOTDIR}"
+
+    cd "${DOTDIR}" \
+      || fail "Could not change into ${DOTDIR}"
+    git submodule update --quiet --init --recursive \
+      || fail "Could not clone submodules in ${DOTDIR}"
   fi
 }
 
 main() {
-  sudo -v
-
+  sudo -v \
+    || fail "error configuring OSX"
+  
   do_dotfilerepo
   install_antigen
   install_dotfiles
   install_powerline
+
+  if ask "Install GDB Peda?" N; then
+    install_peda
+	fi;
 
   if [[ -e ${DOTDIR}/extra ]]; then
     # shellcheck disable=SC1090
     source "${DOTDIR}/extra"
   fi
 
-
   # If we're on a Mac, let's install and setup homebrew.
   if [[ "$(uname -s)" == "Darwin" ]]; then
     info "configuring OSX system"
-    if [[ -f ${DOTDIR}/osx ]]; then
+    #TODO: get latest version from https://mths.be/osx => https://github.com/mathiasbynens/dotfiles/blob/master/.osx
+    if [[ -f ${DOTDIR}/osx ]] \
+      && ask "Set the recommended OSx settings?" N;
+    then
       # shellcheck source=./osx
       if [[ $(source ${DOTDIR}/osx) ]]; then
         success "OSX configured"
@@ -194,7 +269,7 @@ main() {
         fail "error installing GUI tools"
       fi
     fi
-
+    
   fi
 }
 
